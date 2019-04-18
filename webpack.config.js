c21db9d92dd223b29d87fd3df3b45b28c31aa538
RyzeZhao <643080112@qq.com>
2019-04-18 21:53:56 +0800

    9.1    webpack.config.js的entry配置，写成json格式

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
 //因为出口要绝对路径，所以需要引入node的path模块
-const path=require("path");
+const path = require("path");
 //主配置文件，需要暴露在全局（模块的到处command.js node提供的模块方式）
 module.exports = {
-    mode:"development",
+    mode: "development",
     //因为版本问题，现在需要将入口文件放到src下
-    entry:["./src/js/index.js","./src/js/head.js","./src/js/bottom.js"],
-    output:{
-        path:path.resolve(__dirname,"build"),
-        filename:"bundle.js"
+    entry: {
+        "one":"./src/js/index.js", 
+        "two":"./src/js/head.js", 
+        "three":["./src/js/index.js","./src/js/bottom.js",]
+
+    },
+    output: {
+        path: path.resolve(__dirname, "build"),
+        //[name]是会自动遍历entry的名字做为文件
+        filename: "[name].bundle.js"
     }
 }
 
